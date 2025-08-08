7fa374557f1577a9d7a11f729f8e12dbd215cb13
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-08 13:24:10 +0800

    create landing page

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <button class="btn btn-outline btn-accent">
-    Accent
-    <Icon name="tabler:circle-x-filled" size="60" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
