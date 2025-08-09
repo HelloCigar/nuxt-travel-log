dbf5cead37e8da7d4a1408561829e510252219ad
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-09 17:20:45 +0800

    session hook for the client side call for session

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,8 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./db"; // your drizzle instance
 import env from "./env";
-import { en } from "zod/v4/locales";
+import { createAuthMiddleware } from "better-auth/api";
+
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -16,5 +17,18 @@ export const auth = betterAuth({
             clientId: env.GITHUB_CLIENT_ID,
             clientSecret: env.GITHUB_CLIENT_SECRET,
         }
+    },
+    hooks: {
+        after: createAuthMiddleware(async (ctx) => {
+            if (ctx.path === "/get-session") {
+                if(!ctx.context.session) {
+                    return ctx.json({
+                        session: null,
+                        user: null
+                    });
+                }
+            }
+            return ctx.json(ctx.context.session)
+        })
     }
 });
