d5c23ea76e7cfdb1f66c0de3ed09d30bd7a7c72c
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-10 21:40:51 +0800

    sidebar setup done

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,3 +1,55 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const isSideBarOpen = ref(true);
 
-<template><h1>You are logged in!</h1></template>
+onMounted(() => {
+  isSideBarOpen.value = localStorage.getItem("isSideBarOpen") == "true";
+});
+
+function toggleSidebar() {
+  isSideBarOpen.value = !isSideBarOpen.value;
+  localStorage.setItem("isSideBarOpen", isSideBarOpen.value.toString());
+}
+</script>
+
+<template>
+  <div class="flex-1 flex">
+    <div
+      class="bg-base-100 transition-all duration-300"
+      :class="{ 'w-64': isSideBarOpen, 'w-16': !isSideBarOpen }"
+    >
+      <div
+        class="flex hover:cursor-pointer hover:bg-base-200 p-2"
+        :class="{
+          'justify-center': !isSideBarOpen,
+          'justify-end': isSideBarOpen,
+        }"
+        @click="toggleSidebar"
+      >
+        <Icon v-if="isSideBarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton
+          :showLabel="isSideBarOpen"
+          icon="tabler:map"
+          label="Locations"
+          href="/dashboard"
+        />
+        <SidebarButton
+          :showLabel="isSideBarOpen"
+          icon="tabler:circle-plus-filled"
+          label="Add Location"
+          href="/dashboard/add"
+        />
+        <div class="divider"></div>
+        <SidebarButton
+          :showLabel="isSideBarOpen"
+          icon="tabler:logout-2"
+          href="/sign-out"
+          label="Sign Out"
+        />
+      </div>
+    </div>
+    <div class="flex-1"></div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,7 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  icon: {
+    serverBundle: 'local'
+  }
 })
