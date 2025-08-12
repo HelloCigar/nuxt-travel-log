3420c73a585b2cda2245877e6b576638939448cf
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-12 11:41:54 +0800

    chore: consolidate queries to a module

--- lib/db/schema/location-log-image.ts
+++ lib/db/schema/location-log-image.ts
@@ -1,12 +1,21 @@
-import { int, real ,sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { locationLog } from "./location-log";
 import { user } from "./auth";
 
 export const locationLogImage = sqliteTable("locationLogImage", {
   id: int().primaryKey({ autoIncrement: true }),
   key: text().notNull(),
-  locationLogId: int().notNull().references(() => locationLog.id),
-  userId: int().notNull().references(() => user.id),
-  createdAt: int().notNull().$default(() => Date.now()),
-  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()) 
+  locationLogId: int()
+    .notNull()
+    .references(() => locationLog.id),
+  userId: int()
+    .notNull()
+    .references(() => user.id),
+  createdAt: int()
+    .notNull()
+    .$default(() => Date.now()),
+  updatedAt: int()
+    .notNull()
+    .$default(() => Date.now())
+    .$onUpdate(() => Date.now()),
 });
--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,92 +1,84 @@
-import { and, DrizzleError, eq } from "drizzle-orm"
-import slugify from "slug"
-import db from "~~/lib/db"
-import { InsertLocation, location } from "~~/lib/db/schema"
-import { customAlphabet } from 'nanoid'
+import { and, DrizzleError, eq } from "drizzle-orm";
+import slugify from "slug";
+import db from "~~/lib/db";
+import { InsertLocation, location, user } from "~~/lib/db/schema";
+import {
+  findLocationByName,
+  findUniqueSlug,
+  insertLocation,
+} from "~~/lib/db/queries/location";
 
-const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 5) 
+export default defineEventHandler(async (event) => {
+  if (!event.context.user) {
+    return sendError(
+      event,
+      createError({
+        status: 401,
+        statusMessage: "Unauthorized",
+      }),
+    );
+  }
 
-export default defineEventHandler( async (event) => {
-    if (!event.context.user) {
-        return sendError(event, createError({
-            status: 401,
-            statusMessage: "Unauthorized"
-        }))
+  const result = await readValidatedBody(event, InsertLocation.safeParse);
+
+  if (!result.success) {
+    const staturMessage = result.error.issues
+      .map((issue) => `${issue.path.join("")}: ${issue.message}`)
+      .join("; ");
+
+    const data = result.error.issues.reduce(
+      (errors, issue) => {
+        errors[issue.path.join("")] = issue.message;
+        return errors;
+      },
+      {} as Record<string, string>,
+    );
+
+    return sendError(
+      event,
+      createError({
+        statusCode: 422,
+        statusMessage: staturMessage,
+        data,
+      }),
+    );
+  }
+
+  const existingLocation = await findLocationByName(
+    result.data,
+    event.context.user.id,
+  );
+
+  if (existingLocation) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 409,
+        statusMessage: "A location with that name already exists!",
+      }),
+    );
+  }
+
+  const slug = await findUniqueSlug(slugify(result.data.name));
+
+  try {
+    return insertLocation(result.data, slug, event.context.user.id);
+  } catch (e) {
+    const error = e as DrizzleError;
+
+    if (
+      error.cause &&
+      String(error.cause).includes("UNIQUE constraint failed: location.slug")
+    ) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 409,
+          statusMessage:
+            "Slug must be unique (the location name is used to generate the slug).",
+        }),
+      );
     }
-
-    const result = await readValidatedBody(event, InsertLocation.safeParse)
-
-    if(!result.success){
-        const staturMessage = result.error.issues.map((issue) =>
-            `${issue.path.join("")}: ${issue.message}`
-            ).join("; ")
-
-        const data = result
-            .error
-            .issues
-            .reduce((errors, issue) => {
-                errors[issue.path.join("")] = issue.message
-                return errors
-            }, {} as Record<string, string>)
-
-        return sendError(event, createError({
-            statusCode: 422,
-            statusMessage: staturMessage,
-            data
-        }))
-    }
-
-    try {
-        const existingLocation = await db.query.location.findFirst({
-            where: 
-                and(
-                    eq(location.name, result.data.name),
-                    eq(location.userId, event.context.user.id)
-                )
-        })
-
-        if (existingLocation) {
-            return sendError(event, createError({
-                statusCode: 409,
-                statusMessage: "A location with that name already exists!",
-            }))
-        }
-
-
-        let slug = slugify(result.data.name)
-        let existing = !!(await db.query.location.findFirst({
-            where: eq(location.slug, slug)
-        }))
-
-        while (existing) {
-           const id = nanoid();
-           const idSlug = `${slug}-${id}` 
-           existing = !!(await db.query.location.findFirst({
-                where: eq(location.slug, idSlug)
-            }))
-            if(!existing){
-                slug = idSlug
-            }
-        }
-
-        const [created] = await db.insert(location).values({
-            ...result.data,
-            slug,
-            userId: event.context.user.id,
-        }).returning();
-        
-        return created
-
-    } catch (e) {
-        const error = e as DrizzleError
-       
-        if (error.cause && String(error.cause).includes('UNIQUE constraint failed: location.slug')){
-            return sendError(event, createError({
-                statusCode: 409,
-                statusMessage: 'Slug must be unique (the location name is used to generate the slug).',
-            }))
-        }
-        throw error
-    }
-
-})
+    throw error;
+  }
+});
