9f90f78ddd30c04ddb6a66573e19cf673679303b
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-12 13:10:47 +0800

    feat: Use authenticated server handler for all requests

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,13 @@
 import { betterAuth } from "better-auth";
+import type { User } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./db"; // your drizzle instance
 import env from "./env";
 import { createAuthMiddleware } from "better-auth/api";
+
+export type UserWithId =  Omit<User, "id"> & {
+  id: number;
+};
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
