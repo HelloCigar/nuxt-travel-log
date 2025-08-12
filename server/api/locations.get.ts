c9c99e1d45a3462b6a193aacd79470674f88b744
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-12 13:11:21 +0800

    feat: Add saved location fetching

---
+++ server/api/locations.get.ts
@@ -0,0 +1,5 @@
+import { findLocations } from "~~/lib/db/queries/location";
+import defineAuthenticatedEventHandler from "../utils/define-authenticated-event-handler";
+export default defineAuthenticatedEventHandler(async (event) => {
+  return findLocations(event.context.user.id) 
+});
