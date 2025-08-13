053aba60ad646d9a8e331e26a9ceda2d8b2ca912
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-08-13 15:50:53 +0800

    feat: add double-click event on map in addition to the drag

--- app/components/app/map.client.vue
+++ app/components/app/map.client.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { CENTER_MANILA } from "~~/lib/constants";
-import type { LngLat } from "maplibre-gl";
+import type { LngLat, MapMouseEvent } from "maplibre-gl";
+import type { MglEvent } from "@indoorequal/vue-maplibre-gl";
 const zoom = 8;
 
 const colorMode = useColorMode();
@@ -22,21 +23,33 @@ function updateAddedPoint(location: LngLat) {
     mapStore.addedPoint.long = location.lng;
   }
 }
+
+function onDoubleClick(mglEvent: MglEvent<"dblclick">) {
+  if (mapStore.addedPoint) {
+    mapStore.addedPoint.lat = mglEvent.event.lngLat.lat;
+    mapStore.addedPoint.long = mglEvent.event.lngLat.lng;
+  }
+}
 </script>
 
 <template>
-  <MglMap :map-style="style" :center="CENTER_MANILA" :zoom="zoom">
+  <MglMap
+    :map-style="style"
+    :center="CENTER_MANILA"
+    :zoom="zoom"
+    @map:dblclick="onDoubleClick"
+  >
     <MglNavigationControl />
     <MglMarker
       v-if="mapStore.addedPoint"
-      :coordinates="CENTER_MANILA"
+      :coordinates="[mapStore.addedPoint.long, mapStore.addedPoint.lat]"
       @update:coordinates="updateAddedPoint"
       draggable
     >
       <template v-slot:marker>
         <div
           class="tooltip tooltip-top tooltip-open hover:cursor-pointer"
-          data-tip="Drag to your desired location"
+          data-tip="Drag/double-click to your desired location"
         >
           <Icon name="tabler:map-pin-filled" size="30" class="text-warning" />
         </div>
--- app/pages/dashboard/add.vue
+++ app/pages/dashboard/add.vue
@@ -135,6 +135,7 @@ onBeforeRouteLeave(() => {
         <Icon name="tabler:map-pin-filled" class="text-warning" /> marker to
         your desired location
       </p>
+      <p>Or double-click on the map.</p>
       <p class="text-xs text-gray-400">
         Current location: {{ formatNumber(controlledValues.lat) }},
         {{ formatNumber(controlledValues.long) }}
